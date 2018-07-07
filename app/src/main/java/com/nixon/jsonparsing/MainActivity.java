732e74ce0399175f55653b23711db6585fed4c02
Nikita Drygin <nixon3333@gmail.com>
2018-07-07 15:00:39 +0700

    Add loading

--- app/src/main/java/com/nixon/jsonparsing/MainActivity.java
+++ app/src/main/java/com/nixon/jsonparsing/MainActivity.java
@@ -2,6 +2,7 @@ package com.nixon.jsonparsing;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -22,6 +23,9 @@ public class MainActivity extends Activity {
     private RecyclerView recyclerView;
     private ListaPokemonAdapter listaPokemonAdapter;
 
+    private int offset;
+    private boolean aptoParaCargar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,24 +35,49 @@ public class MainActivity extends Activity {
         listaPokemonAdapter = new ListaPokemonAdapter(this);
         recyclerView.setAdapter(listaPokemonAdapter);
         recyclerView.setHasFixedSize(true);
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 3);
+        final GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 3);
         recyclerView.setLayoutManager(gridLayoutManager);
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                if (dy > 0) {
+                    int visibleItemCount = gridLayoutManager.getChildCount();
+                    int totalItemCount = gridLayoutManager.getItemCount();
+                    int pastVisibleItems = gridLayoutManager.findFirstVisibleItemPosition();
+
+                    if (aptoParaCargar) {
+                        if ((visibleItemCount + pastVisibleItems) >= totalItemCount) {
+                            aptoParaCargar = false;
+                            offset += 30;
+                            getData(offset);
+                        }
+                    }
+                }
+            }
+        });
 
         retrofit = new Retrofit.Builder()
                 .baseUrl("http://pokeapi.co/api/v2/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
-        getData();
+
+        aptoParaCargar = true;
+        offset = 0;
+
+        getData(offset);
     }
 
-    private void getData() {
+    private void getData(int offset) {
         PokemonService service = retrofit.create(PokemonService.class);
-        Call<PokemonRespuesta> pokemonRespuestaCall = service.obtenerListaPokemon();
+        Call<PokemonRespuesta> pokemonRespuestaCall = service.obtenerListaPokemon(30, offset);
 
         pokemonRespuestaCall.enqueue(new Callback<PokemonRespuesta>() {
 
             @Override
             public void onResponse(Call<PokemonRespuesta> call, Response<PokemonRespuesta> response) {
+                aptoParaCargar = true;
                 if (response.isSuccessful()) {
                     PokemonRespuesta pokemonRespuesta = response.body();
                     ArrayList<Pokemon> listaPokemon = pokemonRespuesta.getResults();
@@ -61,6 +90,7 @@ public class MainActivity extends Activity {
 
             @Override
             public void onFailure(Call<PokemonRespuesta> call, Throwable t) {
+                aptoParaCargar = true;
                 Log.d(TAG, "onFailure " + t.getMessage());
             }
         }
--- app/src/main/java/com/nixon/jsonparsing/PokemonService.java
+++ app/src/main/java/com/nixon/jsonparsing/PokemonService.java
@@ -2,9 +2,10 @@ package com.nixon.jsonparsing;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface PokemonService {
 
     @GET("pokemon") ///?limit=30&offset=30
-    Call<PokemonRespuesta> obtenerListaPokemon();
+    Call<PokemonRespuesta> obtenerListaPokemon(@Query("limit") int limit, @Query("offset") int offset);
 }
