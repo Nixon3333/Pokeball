2f34cd4cdbc41281e42e629efdf6f7c57b15a5e0
Nikita Drygin <nixon3333@gmail.com>
2018-07-08 12:26:35 +0700

    Not working pictures

--- app/src/main/java/com/nixon/jsonparsing/DetailActivity.java
+++ app/src/main/java/com/nixon/jsonparsing/DetailActivity.java
@@ -1,6 +1,7 @@
 package com.nixon.jsonparsing;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -22,10 +23,13 @@ public class DetailActivity extends Activity {
     private static final String TAG = "detailActivity";
 
     private ImageView imagePoke;
-    private TextView descPoke;
+    private TextView namePoke, heightPoke, weightPoke, typePoke, statsPoke, tvId;
     private PokemonListAdapter pokemonListAdapter;
     private Retrofit retrofit;
+    private Context context;
     private int id;
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,17 +40,25 @@ public class DetailActivity extends Activity {
         Intent intent = getIntent();
         String name = intent.getStringExtra("name");
 
-        /*Glide.with(this)
-                .load("http://pokeapi.co/media/sprites/pokemon/" +id+ ".png")
-                .into(imagePoke);*/
+
 
         retrofit = new Retrofit.Builder()
                 .baseUrl("http://pokeapi.co/api/v2/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
+        namePoke = findViewById(R.id.namePoke);
+        heightPoke = findViewById(R.id.heightPoke);
+        weightPoke = findViewById(R.id.weightPoke);
+        typePoke = findViewById(R.id.typePoke);
+        statsPoke = findViewById(R.id.statsPoke);
+        tvId = findViewById(R.id.tvId);
 
-        descPoke = findViewById(R.id.descPoke);
         getDetail(name);
+
+        Glide.with(this)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+id+".png")
+
+                .into(imagePoke);
 
 
     }
@@ -61,19 +73,36 @@ public class DetailActivity extends Activity {
                 if (response.isSuccessful()) {
                     DetailResponse detailResponse = response.body();
 
-                    ArrayList<Pokemon> pokemons = detailResponse.getForms();
-                    Log.d(TAG, "pokemon: " + pokemons.get(0) );
-                    Pokemon p = pokemons.get(0);
-                    descPoke.setText(String.valueOf(detailResponse.getWeight()));
+                    String name = detailResponse.getName();
+                    String height = detailResponse.getHeight();
+                    String weight = detailResponse.getWeight();
+                    Types[] types = detailResponse.getTypes();
+                    Stats[] stats = detailResponse.getStats();
+
+
+                    id = detailResponse.getId();
+tvId.setText(String.valueOf(id));
+
+
+
+
+
+                    namePoke.setText(name);
+                    heightPoke.setText(height);
+                    weightPoke.setText(weight);
+                    typePoke.setText(String.valueOf(types[0].getType()));
+                    statsPoke.setText(String.valueOf(stats[0]));
+
 
                 }
             }
-                @Override
-                public void onFailure (Call < DetailResponse > call, Throwable t){
-                    Log.d(TAG, "onFailure: " + t.getMessage());
-                }
-            });
-        }
 
+            @Override
+            public void onFailure(Call<DetailResponse> call, Throwable t) {
+                Log.d(TAG, "onFailure: " + t.getMessage());
+            }
+        });
     }
+
+}
 
